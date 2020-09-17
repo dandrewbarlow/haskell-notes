b1c8029879a9aa6041f13c329046dc0130e7a175
Andrew Barlow <dandrewbarlow@gmail.com>
2020-09-17 16:25:42 -0600

    more on lists

--- 5-lists-and-tuples.hs
+++ 5-lists-and-tuples.hs
@@ -1,11 +1,12 @@
 
 -- LISTS & TUPLES
 ---------------------------------------------------------
-
 -- Lists can only have one type, e.g.
 [1, 2, 3, 4, 5] :: [Integer]
 
+
 -- Constructors
+------------------------------------------------------------------
 
 -- Empty List:
 []
@@ -15,7 +16,10 @@
 x:xs
 1 : 2 : 3 : 4 : 5 : []
 
+
+
 -- Ranges
+-------------------------------------------------------------------
 -- generate list the easy way
 [1..5]
 ['a'..'z']
@@ -30,15 +34,30 @@ x:xs
 -- floating points' imprecision can lead to weird range behavior
 -- learn a good haskell lady says not to use them in ranges
 [0.1, 0.3 .. 1]
-=>[0.1,0.3,0.5,0.7,0.8999999999999999,1.0999999999999999]  
+ =>[0.1,0.3,0.5,0.7,0.8999999999999999,1.0999999999999999]  
 
 -- Since Haskell is lazy, we can make infinite lists that it won't try to evaluate
 -- This says to take 24 elements of this infinite list of 13's multiples
 take 24 [13,26..]
 
+-- cycle turns a list into an infinite list
+-- must cut off any access to avoid infinitely listing and infinite list
+take 10 (cycle [1,2,3]) 
+ =>[1,2,3,1,2,3,1,2,3,1]
+
+-- repeat takes an element and produces an infinite list of just that element
+-- same cut off advice applies here
+take 10 repeat 5
+[5,5,5,5,5,5,5,5,5,5]
+
+-- to do this finitely, it is easier to use replicate
+replicate 5 10
+[5,5,5,5,5,5,5,5,5,5]
+
 
 
 -- Generating a List
+----------------------------------------------------------------------
 -- asc - ascending list from n to m
 asc :: Int -> Int -> [Int]
 asc n m
@@ -47,6 +66,7 @@ asc n m
 	| m > n = n : asc (n+1) m
 
 -- library which contains lots of list functionality
+-- useful and neccessary for most work with lists
 import Data.List
 
 -- get the head element of a list
@@ -89,22 +109,68 @@ or [True, False, True]
 	=> True
 
 -- List Comprehensions
+------------------------------------------------------------------------
 -- take a list and build a new list out of it
+-- Similar to set comprehensions
+
+-- This is the basic syntax:
+-- List = [output function | input set, predicate]
+
+
+-- Some different cases:
+
 
 -- take a list and return a list of those elements multiplied by two
-[ 2*x | x <- [1, 2, 3] ] => [2, 4, 6]
+[ 2*x | x <- [1, 2, 3] ]
+ => [2, 4, 6]
 
 -- same as above, except use a guard to filter which elements are used
-[ 2*x | x <- [1, 2, 3], x > 1 ] => [4, 6]
+[ 2*x | x <- [1, 2, 3], x > 1 ]
+ => [4, 6]
+
+-- another example, but this time we are filtering output
+[ 2 * x | x <- [1..6], x * 2 > 10]
+ => [10, 12]
+
+-- an example with a different kind of outut function
+[ if x `mod` 2 then "Even" else "Odd" | x <- [0..10] ]
+ => ["Even", "Odd", "Even", "Odd", "Even", "Odd", "Even", "Odd", "Even", "Odd", "Even"]
 
 -- example with multiple lists being turned into tuples
 [ (x, y) | x <- [1, 2, 3], y <- ['a', 'b'] ]
-	=> [ (1, 'a'), (1, 'b'), (2, 'a'), (2, 'b'), (3, 'a'), (3, 'b') ]
+ => [ (1, 'a'), (1, 'b'), (2, 'a'), (2, 'b'), (3, 'a'), (3, 'b') ]
+
+-- multiple predicates can be used
+[ x | x <- [1..10], x /= 3, x/= 7 ]
+ => [1, 2, 4, 5, 6, 8, 9, 10]
+
+-- so can multiple lists
+-- this will calculate all the values
+[ x*y | x <- [2,5,10], y <- [8,10,11]]
+ => [16,20,22,40,50,55,80,100,110]
+
+-- A fun learn u a good haskell example is 
+let nouns = ["hobo","frog","pope"]  
+let adjectives = ["lazy","grouchy","scheming"]  
+[adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]  
+ => ["lazy hobo","lazy frog","lazy pope","grouchy hobo","grouchy frog", "grouchy pope","scheming hobo","scheming frog","scheming pope"]   
+-- Another example shows an easy way to calculate length of a list
+-- it turns every element in a list into a one and adds them up
+-- wildcard prevents unneccessary assignment of a value we don't care about
+length` xs = sum [1 | _ <- xs]
+
+-- Remove non-lowercase characters from a string
+onlyLowerCase str = [c | c <- str, c `elem` ['a'..'z']
+
 
 -- generally stated, important that this can be done on multiple lists and with multiple guards
 [<gen> | <elem> <- <list>, ..., <guard>, ...]
 
+
+
+
 -- List Patterns
+--------------------------------------------------------------------------
 -- lists also can use pattern matching
 
 -- sum of empty list is zero, sum of list goes through and recursively adds up list
@@ -119,7 +185,10 @@ evens (x:xs)
 	| mod x 2 == 0 = x : evens xs
 	| otherwise = evens xs
 
+
+
 -- Tuples
+--------------------------------------------------------------------------------------
 -- Have multiple elements in a pair, can have two or more elements
 
 -- pattern matching to find first and second vals of a tuple
